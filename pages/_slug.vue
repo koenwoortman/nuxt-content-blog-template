c89aec4c6cb9cc72ede248c94bf5f330725d923a
Koen Woortman <koensw@outlook.com>
2022-05-07 22:10:14 +0200

    Initial commit

---
+++ pages/_slug.vue
@@ -0,0 +1,38 @@
+<template>
+  <article>
+    <h1>{{ post.title }}</h1>
+
+    <nav>
+      <ul>
+        <li v-for="link of post.toc" :key="link.id">
+          <nuxt-link :to="`#${link.id}`">{{ link.text }}</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+
+    <nuxt-content :document="post" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const slug = params.slug
+    const post = await $content(slug).fetch()
+
+    return { post }
+  },
+  head() {
+    return {
+      title: `${this.post.title} | My Blog`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.post.description
+        }
+      ]
+    }
+  },
+}
+</script>
---
+++ pages/page/_page.vue
@@ -0,0 +1,18 @@
+<template>
+  <posts-page
+    :posts="posts"
+    :currentPage="currentPage"
+    :firstPage="firstPage"
+    :lastPage="lastPage"
+  />
+</template>
+
+<script>
+import fetchPostsMixin from '@/utils/fetchPostsMixin'
+import PostsPage from '@/components/PostsPage.vue'
+
+export default {
+  components: { PostsPage },
+  mixins: [fetchPostsMixin]
+}
+</script>
